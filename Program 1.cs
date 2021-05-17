459d71f5577ca9caf9a5e72b526cee1efc6fa7aa
kotkotei <kotkotei@yandex.ru>
2021-05-17 12:24:16 +0700

    jregjetoi

---
+++ Program 1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Варианто 10 задание 1
+            Console.WriteLine("Введиету 2-ух значчисло");
+            string s = Console.ReadLine();
+            int i = 0, j = 1;
+            while (i < s.Length && j < s.Length)
+            {
+                if (s[i] == 'a' || s[j] == 'a')
+                {
+                    Console.WriteLine("Число содержит a");
+                }
+                else
+                {
+                    Console.WriteLine("В числе нет a");
+                }
+                break;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Варианто 10 задание 2
+
+            Console.WriteLine("Ввели x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double k = 0;
+            if (Math.Sin(x) >= 0)
+            {
+                k = Math.Pow(x, 2);
+            }
+            else
+
+
+            {
+                k = Math.Abs(x);
+            }
+            double y = 0;
+            if (x < k)
+            {
+                y = Math.Abs(x);
+            }
+            else
+            {
+                y = k * x;
+            }
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
